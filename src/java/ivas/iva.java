4a604999dc547aefdfcc1fdae1daa0b709b6cf2f
Unknown <aguarinbayo@uniminuto.edu.co>
2018-02-22 13:26:26 -0600

    Archivos inciales
    estos son los archivos de webService

---
+++ src/java/ivas/iva.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ivas;
+import ivas.interfaz.oeraciones;
+import javax.ejb.Stateless;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+
+/**
+ *
+ * @author Claryicon
+ */
+@Stateless
+@WebService(serviceName = "iva")
+
+public class iva implements operaciones {
+    @Override
+    public double calcularIva(@WebParam(name = "calculo") double num){
+        return num*0.19;
+    }
+    
+}
---
+++ src/java/ivas/operaciones.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ivas;
+
+/**
+ *
+ * @author Claryicon
+ */
+public interface operaciones {
+        public double calcularIva(double num);
+}
